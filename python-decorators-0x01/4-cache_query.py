798c9476feba2a6f3366254f825caf46521e4f7c
Loureen Rachael Nangami <rachaelnangami29@gmail.com>
2025-11-15 12:34:42 +0300

    using decorators to cache database queries

---
+++ python-decorators-0x01/4-cache_query.py
@@ -0,0 +1,48 @@
+
+import time
+import sqlite3
+import functools
+
+query_cache = {}
+
+
+def cache_query(func):
+    @functools.wraps(func)
+    def wrapper(conn, query, *args, **kwargs):
+        if query in query_cache:
+            # Return cached result
+            return query_cache[query]
+
+        # Run actual function, store result in cache
+        result = func(conn, query, *args, **kwargs)
+        query_cache[query] = result
+        return result
+    return wrapper
+
+
+
+def with_db_connection(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        conn = sqlite3.connect('database.db')
+        try:
+            result = func(conn, *args, **kwargs)
+        finally:
+            conn.close()
+        return result
+    return wrapper
+
+
+@with_db_connection
+@cache_query
+def fetch_users_with_cache(conn, query):
+    cursor = conn.cursor()
+    cursor.execute(query)
+    return cursor.fetchall()
+
+
+# First call → executes query + caches result
+users = fetch_users_with_cache(query="SELECT * FROM users")
+
+# Second call → retrieves from cache
+users_again = fetch_users_with_cache(query="SELECT * FROM users")
