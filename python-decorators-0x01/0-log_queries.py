cba7dab37d645be2f401bc1b083d5e22c4ec31c0
Loureen Rachael Nangami <rachaelnangami29@gmail.com>
2025-11-15 11:14:13 +0300

    logging database queries

---
+++ python-decorators-0x01/0-log_queries.py
@@ -0,0 +1,27 @@
+import sqlite3
+import functools
+from datetime import datetime 
+
+#### decorator to log SQL queries
+def log_queries(func):
+    @functools.wraps(func)
+    def wrapper(query, *args, **kwargs):
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        print(f"[{timestamp}] Executing SQL Query: {query}")
+        return func(query, *args, **kwargs)
+    return wrapper
+
+
+@log_queries
+def fetch_all_users(query):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+
+#### fetch users while logging the query
+users = fetch_all_users(query="SELECT * FROM users")
+print(users)
